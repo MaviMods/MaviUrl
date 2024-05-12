edc695fcf40ed9e66c2c7f99993dad03185347ac
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-13 00:29:40 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -164,6 +164,16 @@ if len(DOWNLOAD_DIR) == 0:
 elif not DOWNLOAD_DIR.endswith("/"):
     DOWNLOAD_DIR = f'{DOWNLOAD_DIR}/'
 
+AUTHORIZED_CHATS = environ.get('AUTHORIZED_CHATS', '')
+if AUTHORIZED_CHATS:
+    aid = AUTHORIZED_CHATS.split()
+    for id_ in aid:
+        chat_id, *topic_ids = id_.split(':')
+        chat_id = int(chat_id)
+        user_data.setdefault(chat_id, {'is_auth': False})
+        if topic_ids:
+            user_data[chat_id].setdefault('topic_ids', []).extend(map(int, topic_ids))
+
 SUDO_USERS = environ.get('SUDO_USERS', '')
 if len(SUDO_USERS) != 0:
     aid = SUDO_USERS.split()
@@ -579,6 +589,7 @@ if len(MDL_TEMPLATE) == 0:
 
 config_dict = {'ANIME_TEMPLATE': ANIME_TEMPLATE,
                'AS_DOCUMENT': AS_DOCUMENT,
+               'AUTHORIZED_CHATS': AUTHORIZED_CHATS,
                'AUTO_DELETE_MESSAGE_DURATION': AUTO_DELETE_MESSAGE_DURATION,
                'BASE_URL': BASE_URL,
                'BASE_URL_PORT': BASE_URL_PORT,

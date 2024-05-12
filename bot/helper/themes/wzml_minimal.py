3e3e9911bd2d16228ad18793382c04ce918c2aa6
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-12 17:35:54 +0000

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -6,7 +6,7 @@ class WZMLStyle:
     ST_BN1_URL = 'https://t.me/mavibot_support'
     ST_BN2_NAME = 'Update Channel'
     ST_BN2_URL = 'https://t.me/MODSMAVI'
-    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
+    ST_MSG = '''<i>This bot can download all your Mega|Terabox|torrents link to telegram content ```/leech your_link```.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
